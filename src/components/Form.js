35f84968faeaea660950dc24e74c1e7b848b3383
zuizihad <ahmedxihad@gmail.com>
2020-01-17 03:19:27 +0600

    form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,10 @@ class Form extends Component {
     constructor(){
         super()
         this.state ={
-            fname: " ",
-            lname: " ",
-            email: " ",
-            phone: " "
+            fname: "",
+            lname: "",
+            email: "",
+            phone: ""
         }
     }
 
@@ -16,6 +16,33 @@ class Form extends Component {
         let info = event.target.name
         let infoValue = event.target.value
         this.setState({[info]: infoValue})
+
+        if(info === 'fname'){
+            let namePattern =/^([a-zA-Z]){2,30}$/
+            if(!namePattern.test(infoValue)){
+                this.setState({fname: 'first nae is not valid'})
+            }
+        }
+        if(info === 'lname'){
+            let namePattern = /^([a-zA-Z]){2,30}$/
+            if(!namePattern.test(infoValue)){
+                this.setState({lname: 'last nae is not valid'})
+            }
+            
+        }
+        if(info === 'email'){
+            //let emailPattern =/\s+@\s+\.\s+/;
+            let emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+            if(!emailPattern.test(infoValue)){
+                this.setState({email: 'email is not valid'})
+            }
+        }
+        if(info === 'phone'){ 
+            if(!Number(infoValue)){
+                this.setState({phone: 'phone number is not valid'})
+            }
+        }
     }
 
 
@@ -23,10 +50,10 @@ class Form extends Component {
         return (
             <div className="container">
                 <form className="input-field col s4">
-                <p>{this.state.fname}</p>
-                <p>{this.state.lname}</p>
-                <p>{this.state.email}</p>
-                <p>{this.state.phone}</p>
+                <p>first name: {this.state.fname}</p>
+                <p>last name: {this.state.lname}</p>
+                <p>email: {this.state.email}</p>
+                <p>phone: {this.state.phone}</p>
                 <input onChange ={this.changeHandler} type="text" name="fname" placeholder="your first name" ></input>
                 <input onChange ={this.changeHandler} type="text" name="lname" placeholder="your last name" ></input>
                 <input onChange ={this.changeHandler} type="email" name="email" placeholder="your email" ></input>
